79cce2d7306928c3b81f710ac90671ea88826146
Vijayalakshmi-R09 <76153892+Vijayalakshmi-R09@users.noreply.github.com>
2021-06-22 10:22:48 +0530

    Add files via upload

---
+++ W1/Day1/Ex13.js
@@ -0,0 +1,13 @@
+const student=[{studentId:1,studentName:'Remo',address:'no124,selo street',city:'Chennai',marks:40},
+{studentId:2,studentName:'Matt',address:'#24,Ohio street',city:'Bangalore',marks:79},
+{studentId:3,studentName:'Addy',address:'no:45,viola street',city:'Madurai',marks:74},
+{studentId:4,studentName:'Zed',address:'#4050,Parkington street',city:'Bangalore',marks:60},
+{studentId:5,studentName:'Seth',address:'#3,Pulio street',city:'Mysore',marks:68},]
+
+console.log("\n the student details after removing specified records : \n");
+const newstudents = student.filter(student => (student.city !="Bangalore") && (student.city!="Chennai"));
+const stud=newstudents.map((e)=>{
+    console.log(`StudentName:${e.studentName}, City:${e.city}, StudentId:${e.studentId}, Adrress:${e.address}, Marks:${e.marks}`);
+    return e});
+
+
