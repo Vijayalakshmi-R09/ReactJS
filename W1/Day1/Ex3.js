556eda3be6ad32e9111582d424abce25d8525a02
Vijayalakshmi-R09 <76153892+Vijayalakshmi-R09@users.noreply.github.com>
2021-06-24 22:56:03 +0530

    Delete W1/Day1 directory

--- W1/Day1/Ex3.js
+++
@@ -1,5 +0,0 @@
-let str=Number('152');
-let num=String(1000);
-
-console.log("Convert string ': "+str+"' into numeric\n Type of str after convertion: "+typeof(str));
-console.log("Convert Numeric value: "+num+" into string\n Type of num after convertion: "+typeof(num));
--- W1/Day1/Ex7.js
+++
@@ -1,3 +0,0 @@
-var Format = require('dateformat');
-let format_Pastdate=Format(new Date('10 June 2021 09:50:10 UTC'), "yyyy-mm-dd hh:MM:ss");
-console.log("Past date formatted as required" +format_Pastdate.toString());
